a7d9bd145f7c9a119922ef62c8dbc5c7ef1c2fd8
Zack <dukeandjasper@gmail.com>
2020-02-23 01:27:05 -0600

    Cleaned code

--- src/EnemySpawner.java
+++ src/EnemySpawner.java
@@ -20,6 +20,7 @@ public class EnemySpawner {
 	public static void restart() {
 		
 		enemyGrunts.clear();
+		//Fill array with enemies, exact placement can be specified
 		if(Game.selected_level == 1) {
 			enemyGrunts.add(new EnemyGrunt(100f, 100f, true));
 			enemyGrunts.add(new EnemyGrunt(500f, 500f, true));
@@ -30,13 +31,12 @@ public class EnemySpawner {
 	}
 	
 	public static void update(float delta) {
-		
+	//Each frame check if each enemy is alive, and if so, update and draw it.
 		for(EnemyGrunt grunt : enemyGrunts) {
 			if(grunt.getLivingState()) {
 			hvlDraw(hvlQuadc(grunt.xPos, grunt.yPos, EnemyGrunt.GRUNT_SIZE, EnemyGrunt.GRUNT_SIZE), Color.red);
 			grunt.update(delta, Game.player);
 			}
-			System.out.println(grunt.getLivingState());
 		
 		}
 
