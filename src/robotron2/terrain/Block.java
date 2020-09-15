7248c6dcf897d3a1421b82e383dcbac10738801c
KingC <KingC@DESKTOP-DUI9EJL.local>
2020-09-15 04:12:33 -0500

    explained block generation

--- src/robotron2/terrain/Block.java
+++ src/robotron2/terrain/Block.java
@@ -3,3 +3,7 @@ package robotron2.terrain;
 public class Block {
 
 }
+/* 30*30 pixel blocks for a 64*36 block playfield, 4 quadrants of 1920*1080 
+1's designate air, 0 designate randomly generated blocks
+2 designates entrance, 3 designates exit
+*/
